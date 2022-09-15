19d9464d5f4ff778d886631c2b2e479cc4632f8c
dnieln7 <dnieln7@gmail.com>
2022-09-14 19:34:05 -0500

    Added global bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fly_bloc_course/state/cubit/counter/counter_cubit.dart';
 import 'package:fly_bloc_course/ui/router/app_router.dart';
 import 'package:fly_bloc_course/ui/screen/counter_screen.dart';
 
 void main() {
-  runApp(const FlyBloc());
+  runApp(FlyBloc());
 }
 
-class FlyBloc extends StatefulWidget {
-  const FlyBloc({Key? key}) : super(key: key);
+class FlyBloc extends StatelessWidget {
+  FlyBloc({Key? key}) : super(key: key);
 
-  @override
-  State<FlyBloc> createState() => _FlyBlocState();
-}
-
-class _FlyBlocState extends State<FlyBloc> {
   final AppRouter _router = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fly Bloc Course',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.blue,
-          accentColor: Colors.yellowAccent,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Fly Bloc Course',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.blue,
+            accentColor: Colors.yellowAccent,
+          ),
         ),
+        initialRoute: CounterScreen.path,
+        onGenerateRoute: _router.onGenerateRoute,
       ),
-      initialRoute: CounterScreen.path,
-      onGenerateRoute: _router.onGenerateRoute,
     );
-  }
-
-  @override
-  void dispose() {
-    _router.dispose();
-    super.dispose();
   }
 }
--- lib/ui/router/app_router.dart
+++ lib/ui/router/app_router.dart
@@ -1,42 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fly_bloc_course/state/cubit/counter/counter_cubit.dart';
 import 'package:fly_bloc_course/ui/screen/counter_negative_screen.dart';
 import 'package:fly_bloc_course/ui/screen/counter_positive_screen.dart';
 import 'package:fly_bloc_course/ui/screen/counter_screen.dart';
 
 class AppRouter {
-  final CounterCubit _counterCubit = CounterCubit();
-
   Route<Object?>? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case CounterScreen.path:
         return MaterialPageRoute(
-          builder: (context) => BlocProvider.value(
-            value: _counterCubit,
-            child: const CounterScreen(),
-          ),
+          builder: (context) => const CounterScreen(),
         );
       case CounterPositiveScreen.path:
         return MaterialPageRoute(
-          builder: (context) => BlocProvider.value(
-            value: _counterCubit,
-            child: const CounterPositiveScreen(),
-          ),
+          builder: (context) => const CounterPositiveScreen(),
         );
       case CounterNegativeScreen.path:
         return MaterialPageRoute(
-          builder: (context) => BlocProvider.value(
-            value: _counterCubit,
-            child: const CounterNegativeScreen(),
-          ),
+          builder: (context) => const CounterNegativeScreen(),
         );
       default:
         return null;
     }
-  }
-
-  void dispose() {
-    _counterCubit.close();
   }
 }
